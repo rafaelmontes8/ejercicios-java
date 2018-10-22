8dad01d3f0f0a3a7339ec033b76f4ac8a27df200
Rafael Montes Exposito <rafaelmontes192168@gmail.com>
2018-10-22 18:23:02 +0200

    actualizacion ejercicios tema 5

---
+++ ejercicios 5/ejercicio6_5.java
@@ -0,0 +1,11 @@
+public class ejercicio6_5{
+  public static void main (String args[]) {
+    System.out.println("Cuenta del 320 al 160");
+    int numero = 320;
+    do {
+      numero -=20;
+      System.out.print(numero + "\n");
+    } while (numero >= 160);
+  }
+}
+

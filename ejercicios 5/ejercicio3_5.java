031e28bf2eb7b469319942c3d94881d01f95a534
Rafael Montes Exposito <rafaelmontes192168@gmail.com>
2018-10-22 18:28:54 +0200

    actualizacion ejercicios tema 5

---
+++ ejercicios 5/ejercicio3_5.java
@@ -0,0 +1,11 @@
+public class ejercicio3_5{
+  public static void main (String args[]) {
+    System.out.println("Primos de 5 del 0 al 100");
+    int numero = 0;
+    do {
+      numero +=5;
+      System.out.print(numero + "\n");
+    } while (numero != 100);
+  }
+}
+
